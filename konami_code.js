68d132a5661414e8365c6dd4dcd84324dbd29e73
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-05 17:13:30 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ function init() {
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which)
 
-    if (key === index) {
+    if (key === code[index]) {
       index++
 
       if (index === code.length) {
