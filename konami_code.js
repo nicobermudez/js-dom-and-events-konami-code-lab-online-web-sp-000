8b1429b2a42f66990b6c038650364e5aeb57820f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-05 17:11:09 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
 
   let index = 0
-  
+
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which)
 
